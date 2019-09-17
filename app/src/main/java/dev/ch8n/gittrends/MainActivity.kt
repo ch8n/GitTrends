adc1f98c48c7039d842a21d50ca5d14e1683aa57
ch8n <chetan.garg36@gmail.com>
2019-09-18 00:22:17 +0530

    added/corr-libraries
    1.added testing design and material libs 2.added mind map for view deign

--- app/src/main/java/dev/ch8n/gittrends/MainActivity.kt
+++ app/src/main/java/dev/ch8n/gittrends/MainActivity.kt
@@ -1,7 +1,7 @@
 package dev.ch8n.gittrends
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
