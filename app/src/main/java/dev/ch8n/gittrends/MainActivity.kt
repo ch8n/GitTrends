c7519878ac6d7aeb81f059a5477df35527a3606d
ch8n <chetan.garg36@gmail.com>
2019-09-18 00:02:24 +0530

    init/commit

---
+++ app/src/main/java/dev/ch8n/gittrends/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.ch8n.gittrends
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
